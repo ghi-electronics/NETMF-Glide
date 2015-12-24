b50f486c80d2d9c5f1da630a4ca8f4699eb058a7
John Brochue <john.brochue@ghielectronics.com>
2015-12-24 12:08:42 -0500

    Commit for SDK release 2016.1.0.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.3.7.10")]
-[assembly: AssemblyFileVersion("4.3.7.10")]
+[assembly: AssemblyVersion("4.3.8.0")]
+[assembly: AssemblyFileVersion("4.3.8.0")]
