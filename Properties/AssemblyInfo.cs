e98da1648ec6bd8571cbdccb40d9ca48ed678b11
John Brochue <john.brochue@ghielectronics.com>
2016-03-22 12:03:47 -0400

    Updated copyright year.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GHI Electronics")]
 [assembly: AssemblyProduct("GHI.Glide")]
-[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2015")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
